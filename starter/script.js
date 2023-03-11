294e7b1a4f655c6583d100ba6b41860cbdc979fb
Lola <86410482+19Lola89@users.noreply.github.com>
2023-03-11 17:58:59 +0000

    added todays forecast

--- starter/script.js
+++ starter/script.js
@@ -1,16 +1,12 @@
 // load the page when it is fully loaded
 $(document).ready(function () {
-  const queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=";
-  const key = "8a6e247e634be119fd5fd7939e946b46";
   const date = moment().format("llll");
   console.log(date);
   let cities = JSON.parse(localStorage.getItem("cities")) || [];
-  // let queryURL =
-  //   "https://api.openweathermap.org/data/2.5/weather?q=" +
-  //   cityURL +
-  //   "&appid=" +
-  //   APIKey +
-  //   "&units=metric";
+  const queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=";
+  const key = "&appid=b1771496f45c74d965bd4b3150378e81";
+
+  //  lat={lat}&lon={lon}
 
   // DOM tags
 
@@ -18,23 +14,6 @@ $(document).ready(function () {
   let searchInput = $("#search-input");
   let today = $("#today");
   let forecast = $("#forecast");
-
-  $("#search-button").on("click", function (e) {
-    e.preventDefault();
-
-    let cityInput = $("#search-input").val().trim();
-    // conditional statement to add only new cities in my array
-    if (!cities.includes(cityInput) && cityInput !== "") {
-      cities.push(cityInput);
-      // setting up local storage
-      localStorage.setItem("cities", JSON.stringify(cities));
-      // need to add two functions here :  to create a button and to call an API
-      // create a button
-      buttons();
-      // call the api
-      callApi();
-    }
-  });
 
   const buttons = (location) => {
     //empty the group of dynamic elements
@@ -53,25 +32,73 @@ $(document).ready(function () {
 
   buttons();
 
+  $("#search-button").on("click", function (e) {
+    e.preventDefault();
+
+    let cityInput = $("#search-input").val().trim();
+    // conditional statement to add only new cities in my array
+    if (!cities.includes(cityInput) && cityInput !== "") {
+      cities.push(cityInput);
+      // setting up local storage
+      localStorage.setItem("cities", JSON.stringify(cities));
+      // need to add two functions here :  to create a button and to call an API
+      // create a button
+      buttons(cityInput);
+      // call the api
+      callApi(cityInput);
+    }
+  });
   //AJAX call
+
+  // retrieving
 
   const callApi = (value) => {
     $("#today").empty();
-    arr = [];
     $("#forecast").empty();
 
     $.ajax({
       url: queryURL + value + key,
       method: "GET",
-    }).then(function (response) {});
+    }).then(function (response) {
+      // saving api object to a variable
+      // let weatherObj = response;
+
+      let icon = response.list[0].weather[0].icon;
+
+      // dynamically creating elements to get the data on the page
+
+      let i = $("<img>").attr(
+        "src",
+        `http://openweathermap.org/img/wn/${icon}@2x.png`
+      );
+
+      let returnedCity = $("<h2>")
+        .text(response.city.name + "\n" + " " + "(" + date + ")")
+        .append(i);
+
+      let currentTemp = $("<h3>").text(
+        "Temp: " + (response.list[0].main.temp - 273.15).toFixed(2) + "°C"
+      );
+
+      let mph = $("<h3>").text("Wind: " + response.list[0].wind.speed + " MPH");
+
+      let humidity = $("<h3>").text(
+        "Humidity " + response.list[0].main.humidity + " %"
+      );
+
+      $("#today").prepend(returnedCity);
+      $("#today").append(currentTemp, mph, humidity);
+
+      // todays section
+    });
   };
-
-  // $.ajax({
-  //   url: queryURL,
-  //   method: "GET",
-  // }).then(function (response) {
-  //   //
-  // });
-
-  //structure
+  // 5 day forecast
 });
+
+//--- 5 day forecast
+// date
+// icon
+// temperature
+// humidity
+
+// make a dropdown button functionality so that the city can be searched again.
